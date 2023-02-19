8190f352d6d2ada378815b0cc6d72768eeb83867
johnnywhitez <78499293+johnnywhitez@users.noreply.github.com>
2023-02-19 18:09:52 +0000

    Maven Web  App

---
+++ src/main/webapp/edits/js/shopcard.js
@@ -0,0 +1,13 @@
+// Get the HTML element with class "card"
+const card = document.querySelector(".card");
+
+// Create a new <img> element
+const img = document.createElement("img");
+
+// Set the src attribute of the <img> element
+img.setAttribute("src", "edits/img/#");
+
+// Append the <img> element to the "card" element
+card.appendChild(img);
+
+
